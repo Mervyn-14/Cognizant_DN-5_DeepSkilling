e7594da74ec31de8c98b72f81e3c71031f05c8e3
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-15 13:03:26 +0530

    Fix SQL syntax for date range condition

--- Module3_PLSQL/Exercise1_ControlStrucutres/Scenerio3.sql
+++ Module3_PLSQL/Exercise1_ControlStrucutres/Scenerio3.sql
@@ -3,7 +3,7 @@ BEGIN
         SELECT l.LoanID,c.Name, l.EndDate FROM Loans l
         JOIN Customers c
         ON l.CustomerID = c.CustomerID
-        WHERE l.EndDateBETWEEN SYSDATE AND SYSDATE + 30
+        WHERE l.EndDate BETWEEN SYSDATE AND SYSDATE + 30
     )
     LOOP
         DBMS_OUTPUT.PUT_LINE('Reminder: Dear ' || lr.Name || ', your loan ' 
