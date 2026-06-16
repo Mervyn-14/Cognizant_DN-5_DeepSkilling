de83a9d0c92e460e3b453e5cdc0c3e3426ebdaac
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-16 18:22:03 +0530

    Exercise 2 Completed

---
+++ Module-4(Advanced_Junit)/Exercise2_TestSuit/src/test/java/com/example/AllTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    CalculatorTest.class,
+    EvenCheckerTestt.class
+})
+
+public class AllTests {
+}
---
+++ Module-4(Advanced_Junit)/Exercise2_TestSuit/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, 2 + 3);
+    }
+}
