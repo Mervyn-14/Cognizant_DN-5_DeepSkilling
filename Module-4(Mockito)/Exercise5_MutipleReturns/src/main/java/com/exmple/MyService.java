52d11c6f0387ea7d803e445172d9fcaa08baa6da
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-17 14:30:36 +0530

    Completed Multiple Returns

---
+++ Module-4(Mockito)/Exercise5_MutipleReturns/src/main/java/com/exmple/MyService.java
@@ -0,0 +1,11 @@
+package com.exmple;
+
+public class MyService {
+    private ExternalApi api;
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+    public String fetchData() {
+        return api.getData();
+    }
+}
---
+++ Module-4(Mockito)/Exercise5_MutipleReturns/src/test/java/com/exmple/MyserviceTest.java
@@ -0,0 +1,26 @@
+package com.exmple;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MyserviceTest {
+
+    @Test
+    void testMultipleReturns() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData())
+                .thenReturn("First Call")
+                .thenReturn("Second Call")
+                .thenReturn("Third Call");
+
+        MyService service = new MyService(mockApi);
+
+        assertEquals("First Call", service.fetchData());
+        assertEquals("Second Call", service.fetchData());
+        assertEquals("Third Call", service.fetchData());
+    }
+}
