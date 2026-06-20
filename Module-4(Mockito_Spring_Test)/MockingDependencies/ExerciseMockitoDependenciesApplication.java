264c134c99348d5a95521f30cfae75ae56750376
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-20 12:19:55 +0530

    Deleted

--- Module-4(Mockito_Spring_Test)/MockingDependencies/ExerciseMockitoDependenciesApplication.java
+++
@@ -1,9 +0,0 @@
-package com.xample;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class ExerciseMockitoDependenciesApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ExerciseMockitoDependenciesApplication.class,args);
-    }
-}
--- Module-4(Mockito_Spring_Test)/MockingDependencies/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.xample;
-import org.springframework.stereotype.Service;
-@Service
-public class UserService {
-    public User getUserById(Long id) {
-        return new User(id, "Mervyn");
-    }
-}
