c58a42b95c4e283064e554230685f6f4ddf5e55e
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-23 10:04:10 +0530

    Constructor and Setter Injection Completed

---
+++ Module-5(Spring_Core_Maven)/Exercise7_Constructor&SetterInjection/java/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library;
+
+public class BookRepository {
+    public void displayRepository() {
+        System.out.println("BookRepository Bean Loaded");
+    }
+}
---
+++ Module-5(Spring_Core_Maven)/Exercise7_Constructor&SetterInjection/java/BookService.java
@@ -0,0 +1,18 @@
+package com.library;
+public class BookService {
+    private BookRepository bookRepository;
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Executed");
+    }
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Setter Injection Executed");
+    }
+    public void displayService() {
+        System.out.println("BookService Bean Loaded");
+        bookRepository.displayRepository();
+    }
+}
