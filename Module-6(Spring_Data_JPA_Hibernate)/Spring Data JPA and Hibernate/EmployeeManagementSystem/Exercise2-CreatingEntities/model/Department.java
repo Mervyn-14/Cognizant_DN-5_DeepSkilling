d4522d8620b763e7e2daff52aed6775e8d8ea916
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-06-29 12:24:34 +0530

    Created Entities

---
+++ Module-6(Spring_Data_JPA_Hibernate)/Spring Data JPA and Hibernate/EmployeeManagementSystem/Exercise2-CreatingEntities/model/Department.java
@@ -0,0 +1,26 @@
+package com.example.employeemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "employees")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department",
+            cascade = CascadeType.ALL)
+    private List<Employee> employees;
+}
