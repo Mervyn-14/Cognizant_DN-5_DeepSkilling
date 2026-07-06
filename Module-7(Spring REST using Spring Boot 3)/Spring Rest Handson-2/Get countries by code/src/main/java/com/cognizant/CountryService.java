4e3bb3bbe5b0d07482209ef4624c4d53d282ef99
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-07-06 16:40:51 +0530

    Get countries By code Completed

---
+++ Module-7(Spring REST using Spring Boot 3)/Spring Rest Handson-2/Get countries by code/src/main/java/com/cognizant/CountryService.java
@@ -0,0 +1,29 @@
+package com.cognizant;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                context.getBean("countryList", List.class);
+
+        for (Country country : countryList) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+                return country;
+            }
+        }
+
+        return null;
+    }
+}
