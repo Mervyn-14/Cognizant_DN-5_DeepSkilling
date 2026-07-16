73b1b1aa796ee2a763cc2fb4cb0ec9152d565f16
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-07-16 22:04:09 +0530

    Completed Handson 3

---
+++ Module-8(Microservices with SpringBoot)/3-(Microservices composite handson)/Greeting using API and eureka/greet-service/src/main/java/com/cognizant/GreetController.java
@@ -0,0 +1,16 @@
+package com.cognizant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+
+    @GetMapping("/greet")
+    public String greet() {
+
+        return "Hello World!!";
+
+    }
+
+}
