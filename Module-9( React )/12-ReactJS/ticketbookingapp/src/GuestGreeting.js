d0be8ba8724d9c0e5468df0441e033d8875fd981
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-07-19 21:47:16 +0530

    Completed ReactJS handson 12

---
+++ Module-9( React )/12-ReactJS/ticketbookingapp/src/GuestGreeting.js
@@ -0,0 +1,17 @@
+function GuestGreeting() {
+
+    return (
+
+        <div>
+
+            <h1>Please sign up.</h1>
+
+            <h3>Browse available flights.</h3>
+
+        </div>
+
+    );
+
+}
+
+export default GuestGreeting;
