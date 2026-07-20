f5b254ef48daed4a12f7546f63efbdfd7fbea82e
Mervyn Nandhakumar <mervynnanthakumar@gmail.com>
2026-07-20 20:33:38 +0530

    Completed ReactJS handson 15

---
+++ Module-9( React )/15-ReactJS/ticketraisingapp/src/ComplaintRegister.js
@@ -0,0 +1,133 @@
+import React from "react";
+
+class ComplaintRegister extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+
+            ename: "",
+
+            complaint: "",
+
+            NumberHolder: Math.floor(Math.random() * 100) + 1
+
+        };
+
+    }
+
+    handleChange = (event) => {
+
+        this.setState({
+
+            [event.target.name]: event.target.value
+
+        });
+
+    };
+
+    handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        alert(
+            "Thanks " +
+            this.state.ename +
+            "\nYour Complaint was Submitted.\nTransaction ID is: " +
+            this.state.NumberHolder
+        );
+
+    };
+
+    render() {
+
+        return (
+
+            <div style={{ marginTop: "50px", textAlign: "center" }}>
+
+                <h1 style={{ color: "red" }}>
+                    Register your complaints here!!!
+                </h1>
+
+                <form onSubmit={this.handleSubmit}>
+
+                    <table
+                        align="center"
+                        cellPadding="10"
+                    >
+
+                        <tbody>
+
+                            <tr>
+
+                                <td>Name:</td>
+
+                                <td>
+
+                                    <input
+
+                                        type="text"
+
+                                        name="ename"
+
+                                        value={this.state.ename}
+
+                                        onChange={this.handleChange}
+
+                                    />
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+
+                                <td>Complaint:</td>
+
+                                <td>
+
+                                    <textarea
+
+                                        name="complaint"
+
+                                        value={this.state.complaint}
+
+                                        onChange={this.handleChange}
+
+                                    />
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+
+                                <td></td>
+
+                                <td>
+
+                                    <button type="submit">
+                                        Submit
+                                    </button>
+
+                                </td>
+
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+
+                </form>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default ComplaintRegister;
